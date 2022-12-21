7ad94e5085b34f8609706786b3c30d4956c78451
Irene <ivinentmercadal@iesjoanramis.org>
2022-12-21 09:41:59 +0100

    New example

---
+++ example2/exampleCanvas2.js
@@ -0,0 +1,34 @@
+//It receives an identifier of the canvas element and loads the canvas
+//Returns the canvas context or FALSE if it could not be obtained.
+function loadCanvasContext(idCanvas){
+   var element = document.getElementById(idCanvas);
+   if(element && element.getContext){
+      var context = element.getContext('2d');
+      if(context){
+         return context;
+      }
+   }
+   return false;
+}
+
+window.onload = function(){
+  
+  var ctx = loadCanvasContext('mycanvas');
+  var max = 600;
+  if(ctx){
+    ctx.beginPath();
+    ctx.strokeStyle = "#FF0000";
+    ctx.moveTo(max,1);
+    for (i=max; i>1; i-=5) {
+      if((i%2)!=0){
+        ctx.lineTo(i-5,i);        
+      } else{
+        ctx.lineTo(i,i-5);
+      }
+    }
+    ctx.lineTo(1,i);
+    ctx.closePath();
+    ctx.stroke();
+  }
+   
+}
